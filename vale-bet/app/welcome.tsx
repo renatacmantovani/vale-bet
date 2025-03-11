79329690ea93efb92aa0f8bbe816d9ddc9f9a13d
Renata Mantovani <renatacmantovani@gmail.com>
2025-03-11 15:44:28 -0300

    Adicionando primeiras pastas ao repositório, typescript sem funcionar plenamente

---
+++ vale-bet/app/welcome.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+const Welcome = () => {
+  return (
+    <LinearGradient colors={['#0EDFBD', '#C60000']}
+        style={styles.container}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+
+    >
+     <View style={styles.formContainer}>
+        <View style={styles.logoContainer}>
+            <AntDesign style={styles.logo} name="bank" />
+            <Text style={{color: '#FFF', fontSize:32}}>Ecommerce IA</Text>
+            <Text style={{color: '#FFF', fontSize:18, marginBottom: 20}}>Seja Bem vindo!</Text>
+        </View>
+        <TouchableOpacity style={styles.loginButton}><Text style={{color: '#FFF'}}>Login</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.registerButton}><Text style={{color: '#0EDFBD'}}>Registrar</Text></TouchableOpacity>
+    
+    </View>   
+
+    </LinearGradient>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    formContainer: {
+        width: '80%',
+        height: '50%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logoContainer: {
+        color: '#FFF',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+    logo: {
+        fontSize: 100,
+        marginBottom: 20,
+        color: '#FFF',
+    },
+    loginButton: {
+        width: '100%',
+        height: 50,
+        marginBottom: 10,
+        backgroundColor: '#0EDFBD',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    registerButton: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#0EDFBD',
+    },
+
+})
+
+export default Welcome
